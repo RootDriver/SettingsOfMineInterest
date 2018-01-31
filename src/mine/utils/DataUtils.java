9cc77ed51162796c28d327a52cae50af82cc5483
hyx <1073490317@qq.com>
2018-01-31 21:49:25 +0800

    add ingnore

--- src/mine/utils/DataUtils.java
+++ src/mine/utils/DataUtils.java
@@ -1,5 +1,7 @@
 package mine.utils;
 
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
@@ -7,23 +9,22 @@ import java.util.RandomAccess;
 
 public class DataUtils {
 	/**
-	 * This method is to copy data
+	 * This method is to copy data with type Collection
 	 */
-	public static <T> void copy(List<? super T> dest, List<? extends T> src) {
+	public static <T> void copy(Collection<? super T> dest, Collection<? extends T> src) {
 		int srcSize = src.size();
 		if (srcSize > dest.size())
 			throw new IndexOutOfBoundsException("Source does not fit in dest");
 
 		if (srcSize < 10 || (src instanceof RandomAccess && dest instanceof RandomAccess)) {
-			for (int i = 0; i < srcSize; i++)
-				dest.set(i, src.get(i));
-		} else {
-			ListIterator<? super T> di = dest.listIterator();
-			ListIterator<? extends T> si = src.listIterator();
-			for (int i = 0; i < srcSize; i++) {
-				di.next();
-				di.set(si.next());
+			//I mean, if the collection (src,dest),could be visit by index faster than address,
+			//or,it is  ,em...(something like set),small size ,could use this
+			Object[] array = src.toArray();
+			for(int i = 0 ; i < srcSize ; i++){
+				dest.add((T)array[i]);
 			}
+		} else {
+			dest.addAll(src);
 		}
 	}
 
